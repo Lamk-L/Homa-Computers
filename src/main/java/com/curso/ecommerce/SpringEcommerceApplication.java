d872717dfce7a0d659375e4716d9074c95c378e6
Lamk-L <t053300420@unitru.edu.pe>
2023-12-23 17:14:21 -0500

    Mapeo de clases a tablas y relaciones JPA

--- src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
@@ -1,11 +1,9 @@
 package com.curso.ecommerce;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.jdbc.DataSourcePoolMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SpringEcommerceApplication {
 
 	public static void main(String[] args) {
