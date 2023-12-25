9453f276b4132124d68f51d52b5794e046689b8a
Lamk-L <t053300420@unitru.edu.pe>
2023-12-25 01:43:28 -0500

    Mostrar los productos en la home del administrador

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -71,14 +71,12 @@ public class ProductoController {
 
     @PostMapping("/update")
     public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+        Producto p = new Producto();
+        p = productoService.get(producto.getId()).get();
+
         if (file.isEmpty()) { // cuando editamos un producto pero no cambiamos la imagen
-            Producto p = new Producto();
-            p = productoService.get(producto.getId()).get();
             producto.setImagen(p.getImagen());
         } else { // cuando se edita también la imagen
-            Producto p = new Producto();
-            p=productoService.get(producto.getId()).get();
-
             // para eliminar cuando no sea la imagen por defecto
             if (p.getImagen().equals("default.jpg")) {
                 upload.deleteImage(p.getImagen());
@@ -87,6 +85,7 @@ public class ProductoController {
             String nombreImagen = upload.saveImage(file);
             producto.setImagen(nombreImagen);
         }
+        producto.setUsuario(p.getUsuario());
         productoService.update(producto);
         return "redirect:/productos";
     }
