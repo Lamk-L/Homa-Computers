78ab951f564e102c6165f63322e04c094d1dc67d
Lamk-L <t053300420@unitru.edu.pe>
2023-12-25 15:30:10 -0500

    Método para generar el número de la orden

--- src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.curso.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,39 @@ public class OrdenServiceImpl implements IOrdenService {
     @Override
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
+    }
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    public String generarNumeroOrden() {
+        int numero=0;
+
+        List<Orden> ordenes = findAll();
+        
+        List<Integer> numeros = new ArrayList<Integer>();
+        
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        if (ordenes.isEmpty()) {
+            numero = 1;
+        }else {
+            numero = numeros.stream().max(Integer::compare).get();
+            numero++;
+        }
+
+        StringBuilder numeroConcatenado = new StringBuilder();
+        int numeroLongitud = String.valueOf(numero).length();
+        int totalDigitos = 10;  // Total de dígitos deseado en el número
+
+        for (int i = 0; i < totalDigitos - numeroLongitud; i++) {
+            numeroConcatenado.append("0");
+        }
+        numeroConcatenado.append(numero);
+
+        return numeroConcatenado.toString();
     }
 
 }
