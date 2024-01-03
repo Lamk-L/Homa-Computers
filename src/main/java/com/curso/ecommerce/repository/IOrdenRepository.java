f1902060d0f802be35ecfbef01febc79aa0be01a
Lamk-L <t053300420@unitru.edu.pe>
2024-01-03 16:25:59 -0500

    Mostrar las compras para el usuario

--- src/main/java/com/curso/ecommerce/repository/IOrdenRepository.java
+++ src/main/java/com/curso/ecommerce/repository/IOrdenRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.curso.ecommerce.model.Orden;
+import com.curso.ecommerce.model.Usuario;
+
+import java.util.List;
+
 
 @Repository
 public interface IOrdenRepository extends JpaRepository<Orden, Integer> {
+    List<Orden> findByUsuario(Usuario usuario);
 }
--- src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.curso.ecommerce.model.Orden;
+import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.repository.IOrdenRepository;
 
 @Service
@@ -58,6 +59,11 @@ public class OrdenServiceImpl implements IOrdenService {
         numeroConcatenado = String.format("%09d", numero);
     
         return numeroConcatenado;
+    }
+
+    @Override
+    public List<Orden> findByUsuario(Usuario usuario) {
+        return ordenRepository.findByUsuario(usuario);
     }    
 
 }
